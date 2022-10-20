ac2aa8e34f907784077248e8a6b09b9677f29423
Brian Wo <45139213+brainwo@users.noreply.github.com>
2022-10-21 03:25:39 +0800

    feat: add duckscript language

---
+++ coderunner/lang.py
@@ -0,0 +1,46 @@
+LANG = {
+    "python": {
+        "command": "python3",
+        "format": "py"
+    },
+    "py": {
+        "command": "python3",
+        "format": "py"
+    },
+    "jaksel": {
+        "command": "jaksel",
+        "format": "jaksel"
+    },
+    "javascript": {
+        "command": "node",
+        "format": "js"
+    },
+    "js": {
+        "command": "node",
+        "format": "js"
+    },
+    "typescript": {
+        "command": "node",
+        "format": "ts"
+    },
+    "ts": {
+        "command": "node",
+        "format": "ts"
+    },
+    "lua": {
+        "command": "lua",
+        "format": "lua"
+    },
+    "julia": {
+        "command": "julia",
+        "format": "jl"
+    },
+    "ds": {
+        "command": "duck",
+        "format": "ds"
+    },
+    "duckscript": {
+        "command": "duck",
+        "format": "ds"
+    }
+}
